99aebc065c390b6f66944221c02acee6980a3ecc
iibrahimli <imranibrahimli98@gmail.com>
2019-10-16 22:48:50 +0400

    add basic functionality

--- km/include/k_means.hpp
+++ km/include/k_means.hpp
@@ -1,182 +1,201 @@
 #pragma once
 
 #include <vector>
+#include <set>
+#include <random>
+#include <algorithm>
 
 #include "cls_sample.hpp"
 #include "func.hpp"
 
 
-namespace dt{
+namespace km{
 
 
-enum node_type {
-    BRANCH,
-    LEAF
-};
-
-
-struct decision_tree_node {
-
-    node_type                           type;         // type of node
-    std::size_t                         feat_id;      // [BRANCH] index of feature to consider
-    std::size_t                         group_id;     //          group in i_feat to consider
-    std::size_t                         pred;         // [LEAF]   prediction (class_id) if it is a LEAF node
-    std::vector<decision_tree_node *>   branches;     // [BRANCH] branches corresponding to each group
-    std::size_t                         depth;        // depth of node in tree
-
-
-    decision_tree_node()
-        :   type     {LEAF},
-            feat_id  {0},
-            group_id {0},
-            pred     {0},
-            depth    {0}
-    {}
-
-
-    decision_tree_node(node_type tp, std::size_t feat_i = 0, std::size_t group_i = 0, std::size_t prediction = 0, std::size_t depth = 0)
-        :   type     {tp},
-            feat_id  {feat_i},
-            group_id {group_i},
-            pred     {prediction},
-            depth    {depth}
-    {}
-
-};
-
-
-struct decision_tree{
+struct k_means{
 
 private:
 
-    // root node
-    decision_tree_node * root;
+    int                               k;            // number of clusters
 
-    // number of groups for all features (assumend to be the same)
-    std::size_t          n_groups;
+    std::vector<std::vector<float>>   centroids;    // centroids
 
-    // minimum samples (stopping condition)
-     std::size_t         min_samples;
+    int                               max_iter;     // maximum number of iterations
 
-    // max depth (stopping condition)
-    std::size_t          max_depth;
+    float                             delta;        // max diff of centroid between iterations
+
+    float                             min_delta;    // stopping condition
+
+    std::string                       init_method;  // "random" or "kmeans++"
 
 
 
     /*
-        split and add nodes
+        return index of the closest centroid to p
     */
-    template <typename feat_t, std::size_t n_feat>
-    void split(decision_tree_node * cur_node, const std::vector<cls_sample<feat_t, n_feat>> & dataset){
+    template <typename feat_t, std::size_t n_feat>    
+    int closest_centroid(const cls_sample<feat_t, n_feat> & p){
 
-        auto best_feature = get_best_feature(dataset);
+        int   closest;
+        float cl_dist = std::numeric_limits<float>::infinity();
+        float tmp_dist;
 
-        // check stopping condition
-        if(cur_node->depth > max_depth || dataset.size() <= min_samples){
-            // time to stop
-
-            auto [ml, _] = majority_label(dataset);
-
-            // make this node a leaf
-            cur_node->type = LEAF;
-            cur_node->pred = ml;
-        }
-        else{
-
-            // this node is a branch
-            cur_node->type = BRANCH;
-
-            // add child nodes for each group
-            for(int g = 0; g < n_groups; ++g){
-                cur_node->branches.emplace_back(new decision_tree_node(
-                    LEAF,
-                    best_feature,
-                    g,
-                    0,
-                    cur_node->depth + 1
-                ));
+        for(int cl = 0; cl < centroids.size(); ++cl){
+            if((tmp_dist = euclidean_dist(p, centroids[cl])) < cl_dist){
+                closest = cl;
+                cl_dist = tmp_dist;
             }
-
-            // split child nodes
-            for(auto nd : cur_node->branches)
-                split(nd, get_group_as_ds(dataset, nd->feat_id, nd->group_id));
         }
+
+        return closest;
     }
 
 
 public:
 
-    /*
-        default constructor
-        threshold: stopping condition threshold
-                   default sc is (#majority_label / #group_size) > threshold
-    */
-    decision_tree(std::size_t max_depth = 5, std::size_t min_samples = 3)
-        :   root         {nullptr},
-            n_groups     {0},
-            min_samples  {min_samples},
-            max_depth    {max_depth}
-    {}
-
 
     /*
-        builds a decision tree given a dataset (vector of cls_samples)
+        k:               number of clusters
+        init_method:     initialization method ("k++", "random", etc)
     */
-    template <typename feat_t, std::size_t n_feat>
-    void fit(const std::vector<cls_sample<feat_t, n_feat>> & dataset){
-
-        auto ds = dataset;
-        std::vector<float> feat_disc(n_feat, 0);
-        
-        // ASSUMING NUMBER OF GROUPS IS SAME FOR ALL FEATURES
-        n_groups = num_groups(ds, 0);
-        
-        // compute discriminative power for each feature
-        for(auto ftr = 0lu; ftr < n_feat; ++ftr)
-            feat_disc[ftr] = disc(ds, ftr);
-        
-        // make list of features from most disc to least
-        std::vector<std::size_t> features;
-        auto                     feat_tmp = feat_disc;
-
-        // sort features
-        for(int i = 0; i < feat_tmp.size(); ++i){
-            std::size_t max_index = std::distance(feat_tmp.begin(), std::max_element(feat_tmp.begin(), feat_tmp.end()));
-            features.emplace_back(max_index);
-            feat_tmp[max_index] = 0;
-        }
-
-        // --- SPLITTING ---
-
-        root = new decision_tree_node(BRANCH);
-
-        // recursively split nodes until stopping condition is met
-        split(root, ds);
-
+    k_means(int k, int max_iter = 10000, float min_delta = 1e-4, std::string init_method = "kmeans++")
+        :   k           {k},
+            max_iter    {max_iter},
+            delta       {min_delta + 1},
+            min_delta   {min_delta},
+            init_method {init_method}
+    {
+        if(init_method != "kmeans++" && init_method != "random")
+            throw std::invalid_argument("init_method must be one of: \"kmeans++\", \"random\"");
     }
 
 
+    /*
+        fits K-Means on the given dataset (vector of cls_samples)
+    */
+    template <typename feat_t, std::size_t n_feat>
+    int fit(const std::vector<cls_sample<feat_t, n_feat>> & dataset){
+
+        if(dataset.size() <= k)
+            throw std::runtime_error("why on earth would you cluster something like this...");
+
+        // choose initial clusters
+
+        std::random_device rd;
+        std::mt19937 rng(rd());
+        std::uniform_int_distribution<int> uni(0, dataset.size());
+
+        if(init_method == "kmeans++"){
+            // k-means++
+            float dist_closest = 0;
+            float sum_dist     = 0;
+
+            // first centroid - randomly chosen
+            auto  c_idx      = uni(rng);
+            auto  last_c_idx = c_idx;      // sentinel
+            float cdf        = 0;          // cumulative distribution
+
+            // add chosen sample to centroids
+            centroids.emplace_back();
+            centroids[centroids.size()-1].insert(
+                centroids[centroids.size()-1].end(),
+                std::begin(dataset[c_idx]._features),
+                std::end(dataset[c_idx]._features)
+            );
+
+            for(int c = 0; c < k-1; ++c){
+
+                // generate random number (used with cdf to choose centroids according to weights)
+                float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+
+                for(c_idx = 0; c_idx < dataset.size(); ++c_idx){
+
+                    // calculate distance to closest centroid
+                    dist_closest = euclidean_dist(dataset[c_idx], centroids[closest_centroid(dataset[c_idx])]);
+
+                    // calculate sum of distances to closest centroid for all data points
+
+
+                    // choose according to D^2 probability
+                    if(r < 0)
+                        break;
+                }
+
+                // add chosen sample to centroids
+                centroids.emplace_back();
+                centroids[centroids.size()-1].insert(
+                    centroids[centroids.size()-1].end(),
+                    std::begin(dataset[c_idx]._features),
+                    std::end(dataset[c_idx]._features)
+                );
+            }
+
+        }
+        else{
+            std::set<int> chosen_idx;
+
+            for(int c = 0; c < k; ++c){
+                auto c_idx = uni(rng);
+
+                // check whether this index has already been picked
+                if(chosen_idx.find(c_idx) == chosen_idx.end()){
+                    chosen_idx.insert(c_idx);
+
+                    // add chosen sample to centroids
+                    centroids.emplace_back();
+                    centroids[centroids.size()-1].insert(
+                        centroids[centroids.size()-1].end(),
+                        std::begin(dataset[c_idx]._features),
+                        std::end(dataset[c_idx]._features)
+                    );
+                }
+
+            }
+
+        }
+
+        int iter = 0;
+        std::vector<std::size_t> assigned_clusters(dataset.size(), 0);
+
+        for(; iter < max_iter && delta > min_delta; ++iter){
+
+            // calculate which cluster each point belongs to
+            for(int s_idx = 0; s_idx < dataset.size(); ++s_idx){
+                assigned_clusters[s_idx] = closest_centroid(dataset[s_idx]);
+            }
+
+            // re-calculate centroid
+        }
+
+        return iter;
+    }
+
+
+    /*
+        set cluster ids
+        ex: k=3, [2, 0, 1]
+    */
+    void set_cluster_ids(std::vector<std::size_t> order){
+        // std::iter_swap(begin + n, begin + k)
+        for(int i = 0; i < order.size(); ++i){
+            std::iter_swap(centroids.begin() + i, centroids.begin() + order[i]);
+        }
+    }
+    
+    
     /*
         returns predicted class_id
     */
     template <typename feat_t, std::size_t n_feat>
     std::size_t predict(const cls_sample<feat_t, n_feat> & sample){
 
-        auto         cur_node = root;
-        std::size_t  feat;
-        std::size_t  grp;
-
-        while(cur_node->type != LEAF){
-            feat = cur_node->feat_id;
-            cur_node = cur_node->branches[sample[feat]];
-        }
-
-        return cur_node->pred;
+        return closest_centroid(sample);
     }
 
 
     /*
-        builds a decision tree given a dataset (vector of cls_samples)
+        evaluates the clusters given a dataset (vector of cls_samples)
+        NB: set_cluster_ids before calling this
     */
     template <typename feat_t, std::size_t n_feat>
     void evaluate(const std::vector<cls_sample<feat_t, n_feat>> & dataset){
@@ -192,10 +211,18 @@ public:
             if(gt[i] == pred[i]) ++correct_pred;
         }
 
-        std::cout << std::fixed << std::left << std::setw(25) << "test samples: " << std::setw(5) << dataset.size() << std::endl;
+        std::cout << std::fixed << std::left << std::setw(25) << "samples: " << std::setw(5) << dataset.size() << std::endl;
         std::cout << std::fixed << std::left << std::setw(25) << "correct predictions: " << std::setw(5) << correct_pred << std::endl;
         std::cout << std::fixed << std::left << std::setw(25) << "accuracy: " << std::setw(5) << std::setprecision(2) << (float) correct_pred / dataset.size() << std::endl;
 
+    }
+
+
+    /*
+        get cluster centroids
+    */
+    std::vector<std::vector<float>> get_centroids(){
+        return centroids;
     }
 
 
